6e08fe33795abaaaede96821f9534c95daa21e40
Matheus Reusch <matheusreusch1@gmail.com>
2022-03-07 14:37:14 -0300

    Login Page

---
+++ src/Login.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import propTypes from 'prop-types';
+
+function Login(props) {
+  const [disabled, setDisabled] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { history } = props;
+
+  useEffect(() => {
+    const six = 6;
+    if (email.includes('@') && email.includes('.com') && password.length > six) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [email, password]);
+  return (
+    <>
+      <input
+        type="text"
+        value={ email }
+        onChange={ (e) => setEmail(e.target.value) }
+        data-testid="email-input"
+      />
+      <input
+        type="password"
+        onChange={ (e) => setPassword(e.target.value) }
+        value={ password }
+        data-testid="password-input"
+      />
+      <button
+        disabled={ disabled }
+        onClick={ () => {
+          localStorage.setItem('mealsToken', 1);
+          localStorage.setItem('cocktailsToken', 1);
+          const user = {
+            email,
+          };
+          localStorage.setItem('user', JSON.stringify(user));
+          history.push('/foods');
+        } }
+        type="button"
+        data-testid="login-submit-btn"
+      >
+        Enter
+      </button>
+    </>
+  );
+}
+
+Login.propTypes = {
+  history: propTypes.func.isRequired,
+};
+
+export default Login;
