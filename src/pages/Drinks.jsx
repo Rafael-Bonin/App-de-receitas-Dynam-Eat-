41bb31d583aa1415c2b85069607c0479388bf570
Matheus Reusch <matheusreusch1@gmail.com>
2022-03-07 20:41:47 -0300

    Categories

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,15 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import recipesContext from '../services/recipesContext';
 import Card from '../components/Card';
+import GetCategories from '../components/GetCategories';
 
 function Drinks() {
-  const { recipes } = useContext(recipesContext);
+  const { recipes, setRecipes } = useContext(recipesContext);
   const TWELVE = 12;
+
+  useEffect(() => {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
+      .then((response) => response.json())
+      .then((data) => setRecipes(data.drinks));
+  }, []);
   return (
     <div>
       <Header title="Drinks" sb />
+      <GetCategories apiName="thecocktaildb" />
       {recipes.length > 1
         && recipes.map((recipe, index) => (
           index < TWELVE
