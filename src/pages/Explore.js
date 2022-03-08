529b45527240742b5f194cb0e15d9f7c2d3f0808
Rafael <rafalyf11@gmail.com>
2022-03-08 16:56:52 -0300

    requisito 67, 68, 69

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,6 +7,12 @@ export default function Explore() {
   return (
     <div>
       <Header title="Explore" sb={ false } />
+      <Link data-testid="explore-foods" to="/explore/foods">
+        Explore Foods
+      </Link>
+      <Link data-testid="explore-drinks" to="/explore/drinks">
+        Explore Drinks
+      </Link>
       <Footer />
     </div>
   );
