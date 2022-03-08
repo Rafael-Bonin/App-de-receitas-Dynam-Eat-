7cacbac5252e0196cc2208699aa8a7f12c0d2a88
Rafael <rafalyf11@gmail.com>
2022-03-08 16:03:10 -0300

    requisito 82

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,6 +6,12 @@ export default function Profile() {
   return (
     <>
       <Header title="Profile" sb={ false } />
+      <main>
+        <h3 data-testid="profile-email">Email</h3>
+        <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+        <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+        <button type="button" data-testid="profile-logout-btn">LogOut</button>
+      </main>
       <Footer />
     </>
   );
