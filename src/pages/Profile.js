0253ef3afe8ae519020857834a2570f15d746456
Rafael <rafalyf11@gmail.com>
2022-03-08 16:22:10 -0300

    requisito 83 com erro de lint

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Profile() {
+  const [email, setEmail] = useState('');
+  const getEmail = async () => {
+    const Email = await JSON.parse(localStorage.getItem('user'));
+    setEmail(Email.email);
+  };
+  useEffect(() => {
+    getEmail();
+  }, []);
+
   return (
     <>
       <Header title="Profile" sb={ false } />
       <main>
-        <h3 data-testid="profile-email">Email</h3>
+        <h3 data-testid="profile-email">{ email.length > 0 ? (
+          email
+        ) : (
+          <p>Loading</p>
+        )
+         }
+        </h3>
         <button type="button" data-testid="profile-done-btn">Done Recipes</button>
         <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
         <button type="button" data-testid="profile-logout-btn">LogOut</button>
