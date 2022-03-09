99e19b2480547996fb680eac34d626aa646dd512
Rafael <rafalyf11@gmail.com>
2022-03-09 17:42:31 -0300

    inicio requisito 75 e 74 funcionando

--- src/pages/FoodIngredients.js
+++ src/pages/FoodIngredients.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import FetchIngredients from '../services/FetchIngredients';
 
 export default function FoodIngredients() {
+  const [ready, setReady] = useState(false);
+  const [allIngredients, setAllIngredients] = useState();
+
+  const getIngredients = async () => {
+    const ingredients = await FetchIngredients();
+    setAllIngredients(ingredients);
+  };
+
+  useEffect(() => {
+    getIngredients();
+  }, []);
+
+  useEffect(() => {
+    setReady(true);
+    console.log(allIngredients);
+    console.log(ready);
+  }, [allIngredients]);
+
   return (
     <>
       <Header title="Explore Ingredients" sb={ false } />
--- src/services/FetchRandomFood.js
+++ src/services/FetchRandomFood.js
@@ -1,10 +1,8 @@
 const FetchRandomFood = async () => {
-  const getFoods = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+  const getFoods = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
   const processFoods = await getFoods.json();
-  const foods = await processFoods.meals;
-  const random = Math.floor(Math.random() * (foods.length - 0) + 0);
-  const randomFood = await foods[random];
-  return randomFood;
+  const foods = await processFoods.meals[0];
+  return foods;
 };
 
 export default FetchRandomFood;
